b137abf2d51bfbd86818fae242a65db8686bcb5d
hussnainwithss <21100150@lums.edu.pk>
2020-06-05 17:31:24 +0500

    added linter and prettier

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com)
 
 # adopt-me
- Pet Adoption app made using React.JS
 
-
+Pet Adoption app made using React.JS
--- src/App.js
+++ src/App.js
@@ -1,47 +1,40 @@
 const logo = () => {
-    //logo gets automatically rendered using css
-    return React.createElement("h1");
+  //logo gets automatically rendered using css
+  return React.createElement("h1");
 };
 
-const Pet = ({name,breed, animal}) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {key: 5}, name),
-        React.createElement("h2", {key: 6}, animal),
-        React.createElement("h3", {key: 7}, breed),
-    ]);
-}
+const Pet = ({ name, breed, animal }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { key: 5 }, name),
+    React.createElement("h2", { key: 6 }, animal),
+    React.createElement("h3", { key: 7 }, breed),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement(logo, {key:1}),
-            React.createElement(Pet, { 
-                key: 2,
-                name: "Luna", 
-                animal: "Dog", 
-                breed: "Havanese" 
-            }),
+  return React.createElement("div", {}, [
+    React.createElement(logo, { key: 1 }),
+    React.createElement(Pet, {
+      key: 2,
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
 
-            React.createElement(Pet, { 
-                key:3,
-                name: "pepper", 
-                animal: "Bird", 
-                breed: "Cockatiel" 
-            }),
+    React.createElement(Pet, {
+      key: 3,
+      name: "pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
 
-            React.createElement(Pet, { 
-                key:4,
-                name: "Doink", 
-                animal: "Cat", 
-                breed: "Mixed" 
-            }),
-        ]
-    );
+    React.createElement(Pet, {
+      key: 4,
+      name: "Doink",
+      animal: "Cat",
+      breed: "Mixed",
+    }),
+  ]);
 };
 
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById('root')
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
