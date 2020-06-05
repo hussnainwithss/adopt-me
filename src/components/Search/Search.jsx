123135a529de2e7db2ff1022bb485ad062ccc48d
hussnainwithss <21100150@lums.edu.pk>
2020-06-05 22:16:26 +0500

    Added Dropdown component and custom hooks

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "../useDropdown/useDropdown.jsx";
 
 const Search = () => {
   const [location, setLocation] = useState("Seattle, WA"); // API Used only supports only Seattle and San fransisco
-
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
   return (
     <div className="search-params">
       <form>
@@ -15,6 +19,8 @@ const Search = () => {
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button> Submit </button>
       </form>
     </div>
