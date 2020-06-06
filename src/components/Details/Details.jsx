5ee9bac6d99c0b7bffc38ed7ecab97785ad2aabf
hussnainwithss <21100150@lums.edu.pk>
2020-06-06 14:25:18 +0500

    Update Details.jsx
    used class based components for Details just to learn and see how it differs from function based componenets

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,11 +1,37 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = (props) => {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 5)}</code>
-    </pre>
-  );
-};
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //setting initial state to loading so it can fetch data
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    /** Added Mount life cycle method which fetchs data from Pet API using AJAX request
+     * Runs only on Mount
+     * Then never runs again
+     */
+    pet.animal(this.props.id).then(
+      ({ animal }) => {
+        console.log(animal.breeds);
+        this.setState({
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          description: animal.description,
+          media: animal.photos,
+          breed: animal.breeds.primary,
+          loading: false,
+        });
+      },
+      (err) => console.error(err)
+    );
+  }
+  render() {}
+}
 
 export default Details;
